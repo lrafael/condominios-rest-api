cdceb649f20823cf5c0ae88c35918faec97b3b0d
Andrey Luiz <andreypelegrini2@gmail.com>
2018-05-14 18:56:23 -0300

    Adicionado classe de testes unitários. Testando permissao.

---
+++ src/test/java/com/condominios/api/CondominiosAPITest.java
@@ -0,0 +1,14 @@
+package com.condominios.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CondominiosAPITest {
+
+	@Test
+	public void testAssertEquals() {
+		assertEquals(1, 1);
+	}
+
+}
